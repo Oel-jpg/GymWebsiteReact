d15cc79b644601fbe02c75f19e784f8de35a4ff2
Joel <gjmm1226@gmail.com>
2025-06-22 20:01:39 -0600

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="bg-gray-800 text-white p-4">
+      <h1 className="text-lg font-bold">Gym Website</h1>
+    </div>
+  )
+}
+
+export default Navbar
